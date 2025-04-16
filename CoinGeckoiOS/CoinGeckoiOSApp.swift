3fb6d5ade4350b978ef0d407c6dc2a2419b0dae2
Christian Manzaraz <magnusmanz@icloud.com>
2025-04-16 19:30:35 -0300

    Initial Commit

---
+++ CoinGeckoiOS/CoinGeckoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGeckoiOSApp.swift
+//  CoinGeckoiOS
+//
+//  Created by Christian Manzaraz on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinGeckoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinGeckoiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinGeckoiOS
+//
+//  Created by Christian Manzaraz on 16/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
